1c171d3d5318dd1ef718963398c595d1fa84261d
Junior <JuniorFFSJ@Outlook.com>
2024-11-04 23:03:22 -0300

    Sistema de Funcionários e Projetos

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ProjetoLPOOE1_FranciscoJunior
+
+Avaliação da disciplina de LPOO - Linguagem de Programação Orientada a Objetos
+
+## Modelagem
+
+A modelagem do sistema incluiu as seguintes entidades e seus relacionamentos:
+
+![diagrama](img/diagrama.png)
---
+++ src/main/java/br/edu/ifsul/cc/lpoo/sistemacompeticaoesportiva/projetolpooe1_franciscojunior/dao/PersistenciaJPA.java
@@ -0,0 +1,77 @@
+package br.edu.ifsul.cc.lpoo.sistemacompeticaoesportiva.projetolpooe1_franciscojunior.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenciaJPA implements InterfaceBD {
+
+    EntityManager entity;
+    EntityManagerFactory factory;
+
+    public PersistenciaJPA() {
+        //parametro: é o nome da unidade de persistencia (Persistence Unit)
+        factory
+                = Persistence.createEntityManagerFactory("pu_projetolpooe1_franciscojunior");
+        //conecta no bd e executa a estratégia de geração.
+        entity = factory.createEntityManager();
+    }
+
+    @Override
+    public Boolean conexaoAberta() {
+
+        return entity.isOpen();
+    }
+
+    @Override
+    public void fecharConexao() {
+        entity.close();
+    }
+
+    @Override
+    public Object find(Class c, Object id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void persist(Object o) throws Exception {
+        entity = getEntityManager();
+        try {
+            entity.getTransaction().begin();
+            entity.persist(o);
+            entity.getTransaction().commit();
+        } catch (Exception e) {
+            if (entity.getTransaction().isActive()) {
+                entity.getTransaction().rollback();
+            }
+        }
+    }
+
+    @Override
+    public void remover(Object o) throws Exception {
+        entity = getEntityManager();
+        try {
+            entity.getTransaction().begin();
+            entity.remove(o);
+            entity.getTransaction().commit();
+        } catch (Exception e) {
+            if (entity.getTransaction().isActive()) {
+                entity.getTransaction().rollback();
+            }
+        }
+    }
+
+    /*
+    Todos os métodos agora chamam getEntityManager() 
+    para garantir que o EntityManager esteja sempre aberto e 
+    pronto para uso.
+     */
+    public EntityManager getEntityManager() {
+        if (entity == null || !entity.isOpen()) {
+            entity = factory.createEntityManager();
+        }
+        return entity;
+    }
+    
+}
